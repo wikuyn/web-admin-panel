4ba07eaa38e0bede2cdee4492cf4259c04dcac8d
wiku <wikundaru3@gmail.com>
2022-06-25 10:56:22 +0700

    category-page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -72,6 +72,47 @@ module.exports = {
     }
   },
 
+  categoryPage: async (req, res) => {
+    try {
+      const category = await Category.find()
+        .select("_id name")
+        .populate({
+          path: "itemId",
+          select: "_id title country city isPopular  imageId",
+          perDocumentLimit: 4,
+          option: { sort: { sumBooking: -1 } },
+          populate: {
+            path: "imageId",
+            select: "_id imageUrl",
+            perDocumentLimit: 1,
+          },
+        });
+
+      const treveler = await Treveler.find();
+      const treasure = await Treasure.find();
+      const city = await Item.find();
+
+      for (let i = 0; i < category.length; i++) {
+        for (let x = 0; x < category[i].itemId.length; x++) {
+          const item = await Item.findOne({ _id: category[i].itemId[x]._id });
+          item.isPopular = false;
+          await item.save();
+          if (category[i].itemId[0] === category[i].itemId[x]) {
+            item.isPopular = true;
+            await item.save();
+          }
+        }
+      }
+
+      res.status(200).json({
+        category,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
   detailPage: async (req, res) => {
     try {
       const { id } = req.params;
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const apiController = require("../controllers/apiController");
 const { uploadSingle } = require("../middlewares/multer");
 
 router.get("/landing-page", apiController.landingPage);
+router.get("/category-page", apiController.categoryPage);
 router.get("/detail-page/:id", apiController.detailPage);
 router.post("/booking-page", uploadSingle, apiController.bookingPage);
 module.exports = router;
